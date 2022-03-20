94dddc54cf6c3cd0c9c4d95e30cd2dcb7efbc585
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-03-20 20:39:08 +0300

    Initial commit

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import { Routes, Route, HashRouter } from "react-router-dom";
+import { IndexPage } from "./pages";
+import { PopupPage } from "./pages/popup";
+
+export const Router: FC = () => {
+	return (
+		<HashRouter>
+			<Routes>
+				<Route path="/">
+					<Route index element={<IndexPage />} />
+					<Route path="popup" element={<PopupPage />} />
+				</Route>
+			</Routes>
+		</HashRouter>
+	);
+};
